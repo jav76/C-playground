d8d6257a3f6a14f61eb0039e6344d5cd5943315d
Jaret <jav76@uakron.edu>
2022-01-06 18:49:03 -0500

    Start of chapter 1 & 2

---
+++ Chapter02-vscode/Variables/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+object height = 1.88;
+object name = "Amir";
+Console.WriteLine($"{name} is {height} meters tall.");
+
+int length =  ((string)name).Length;
+Console.WriteLine($"{name} has {length} characters.");
+
+/*
+• L: infers long
+• UL: infers ulong
+• M: infers decimal
+• D: infers double
+• F: infers float
+*/
+
+Console.WriteLine($"default(int) = {default(int)}");
+Console.WriteLine($"default(bool) = {default(bool)}");
+Console.WriteLine($"default(DateTime) = {default(DateTime)}");
+Console.WriteLine($"default(string) = {default(string)}");
+
+
+string[] names;
+names = new string[4];
+names[0] = "Kate";
+names[1] = "Jack";
+names[2] = "Rebecca";
+names[3] = "Tom";
+
+for (int i = 0; i < names.Length; i++)
+{
+    Console.WriteLine(names[i]);
+}
