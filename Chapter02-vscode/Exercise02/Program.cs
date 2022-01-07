996a95bcd3d3d76dca40e30703563b447427d186
Jaret <jav76@uakron.edu>
2022-01-07 16:59:45 -0500

    Chapter 2

---
+++ Chapter02-vscode/Exercise02/Program.cs
@@ -0,0 +1,45 @@
+﻿using static System.Console;
+using System.Collections.Generic;
+using System.Globalization;
+
+
+Console.WriteLine(String.Concat(Enumerable.Repeat("-", 100)));
+
+Console.WriteLine(
+    "{0, -12}{1, -20}{2, 34}{3, 34}",
+    "Type", "Byte(s) of memory", "Min", "Max");
+
+Console.WriteLine(
+    "{0, -12}{1, -20}{2, 34}{3, 34}",
+    "sbyte", sizeof(sbyte), sbyte.MinValue, sbyte.MaxValue);
+Console.WriteLine(
+    "{0, -12}{1, -20}{2, 34}{3, 34}",
+    "byte", sizeof(byte), byte.MinValue, byte.MaxValue);
+Console.WriteLine(
+    "{0, -12}{1, -20}{2, 34}{3, 34}",
+    "short", sizeof(short), short.MinValue, short.MaxValue);
+Console.WriteLine(
+    "{0, -12}{1, -20}{2, 34}{3, 34}",
+    "ushort", sizeof(ushort), ushort.MinValue, ushort.MaxValue);
+Console.WriteLine(
+    "{0, -12}{1, -20}{2, 34}{3, 34}",
+    "int", sizeof(int), int.MinValue, int.MaxValue);
+Console.WriteLine(
+    "{0, -12}{1, -20}{2, 34}{3, 34}",
+    "uint", sizeof(uint), uint.MinValue, uint.MaxValue);
+Console.WriteLine(
+    "{0, -12}{1, -20}{2, 34}{3, 34}",
+    "long", sizeof(long), long.MinValue, long.MaxValue);
+Console.WriteLine(
+    "{0, -12}{1, -20}{2, 34}{3, 34}",
+    "ulong", sizeof(ulong), ulong.MinValue, ulong.MaxValue);
+Console.WriteLine(
+    "{0, -12}{1, -20}{2, 34}{3, 34}",
+    "float", sizeof(float), float.MinValue, float.MaxValue);
+Console.WriteLine(
+    "{0, -12}{1, -20}{2, 34}{3, 34}",
+    "double", sizeof(double), double.MinValue, double.MaxValue);
+Console.WriteLine(
+    "{0, -12}{1, -20}{2, 34}{3, 34}",
+    "decimal", sizeof(decimal), decimal.MinValue, decimal.MaxValue);
+
--- Chapter02-vscode/Formatting/Program.cs
+++ Chapter02-vscode/Formatting/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+int numberOfApples = 12;
+decimal pricePerApple = 0.35M;
+Console.WriteLine(
+    format: "{0} apples costs {1:C}",
+    arg0: numberOfApples,
+    arg1: pricePerApple * numberOfApples);
+string formatted = string.Format(
+    format: "{0} apples costs {1:C}",
+    arg0: numberOfApples,
+    arg1: pricePerApple * numberOfApples);
+
+
+string applesText = "Apples";
+int applesCount = 1234;
+string bananasText = "Bananas";
+int bananasCount = 56789;
+Console.WriteLine(
+    format: "{0,-10} {1,6:N0}",
+    arg0: "Name",
+    arg1: "Count");
+Console.WriteLine(
+    format: "{0,-10} {1,6:N0}",
+    arg0: applesText,
+    arg1: applesCount);
+Console.WriteLine(
+    format: "{0,-10} {1,6:N0}",
+    arg0: bananasText,
+    arg1: bananasCount);
+
+
+Console.Write("Type your first name and press ENTER: ");
+string? firstName = Console.ReadLine();
+Console.Write("Type your age and press ENTER: ");
+string? age = Console.ReadLine();
+Console.WriteLine(
+    $"Hello {firstName}, you look good for {age}.");
+
+
+
+Write("Press any key combination: ");
+ConsoleKeyInfo key = ReadKey();
+WriteLine();
+WriteLine("Key: {0}, Char: {1}, Modifiers: {2}",
+    arg0: key.Key,
+    arg1: key.KeyChar,
+    arg2: key.Modifiers);
