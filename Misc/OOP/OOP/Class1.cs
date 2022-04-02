e59f971c693780a6ae81d517db5fdd21873ebf6d
Jaret <jav76@uakron.edu>
2022-04-01 21:45:03 -0400

    Shape objects
    Testing some stuff

---
+++ Misc/OOP/OOP/Class1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public class Square
+    {
+        public string ShapeName { get; private set; }
+        public double Area { get; private set; }
+        public double Perimeter { get; private set; }
+        public string CustomName { get; set; }
+
+        private double _length;
+        public double Length
+        {
+            get
+            {
+                return this._length;
+            }
+            set
+            {
+                this._length = value;
+                this.Area = this.Length * this.Length;
+                this.Perimeter = this.Length * 4;
+            }
+        }
+
+        public double getArea()
+        {
+            Console.WriteLine($"{this.ShapeName} {this.CustomName} area is {this.Area}");
+            return this.Area;
+        }
+
+        public double getPerimeter()
+        {
+            Console.WriteLine($"{this.ShapeName} {this.CustomName} perimeter is {this.Perimeter}");
+            return this.Perimeter;
+        }
+
+
+        public Square(double length)
+        {
+            this.Length = length;
+            this.ShapeName = "Square";
+        }
+    }
+
+    
+    public class Circle
+    {
+        public string ShapeName { get; private set; }
+        public double Radius { get; private set; }
+        public double Area { get; private set; }
+        public double Circumference { get; private set; }
+
+        public string CustomName { get; set; }
+        private double _diameter;
+        public double Diameter
+        { 
+            get
+            {
+                return this._diameter;
+            }
+            set
+            {
+                this._diameter = value;
+                this.Radius = value / 2;
+                this.Area = Math.PI * Math.Pow(this.Radius, 2);
+                this.Circumference = Math.PI * 2 * this.Radius;
+            }
+        }
+
+        public Circle(double diameter)
+        {
+            this.Diameter = diameter;
+            this.ShapeName = "Circle";
+        }
+
+    }
+    
+}
---
+++ Misc/OOP/OOP/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Shapes;
+
+namespace MyApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Shapes.Square mySquare = new Shapes.Square(5);
+
+            mySquare.CustomName = "Custom Square";
+            mySquare.getArea();
+            mySquare.getPerimeter();
+
+            mySquare.Length = 12;
+            mySquare.getArea();
+            mySquare.getPerimeter();
+
+            Shapes.Circle myCircle = new Shapes.Circle(5);
+            Console.WriteLine(myCircle.Circumference);
+            myCircle.Diameter = 6;
+            Console.WriteLine(myCircle.Circumference);
+        }
+    }
+}
